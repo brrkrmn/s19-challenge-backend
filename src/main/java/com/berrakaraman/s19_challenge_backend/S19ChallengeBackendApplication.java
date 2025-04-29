aeb52ca57a155033b1a85ff5f3fbfad37a2fa7b5
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-04-29 13:23:06 +0300

    initialize spring boot

---
+++ src/main/java/com/berrakaraman/s19_challenge_backend/S19ChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.berrakaraman.s19_challenge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S19ChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S19ChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berrakaraman/s19_challenge_backend/S19ChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berrakaraman.s19_challenge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S19ChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
