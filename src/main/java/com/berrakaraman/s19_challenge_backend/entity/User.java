2c861076fa8a07f977d45196bf7bae0afacc1f71
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-04-29 14:36:42 +0300

    create user entity with relations

---
+++ src/main/java/com/berrakaraman/s19_challenge_backend/entity/User.java
@@ -0,0 +1,147 @@
+package com.berrakaraman.s19_challenge_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "about")
+    private String about;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(
+            name = "user_following",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "following_id")
+    )
+    private Set<User> following = new HashSet<>();
+
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, mappedBy = "following")
+    private Set<User> followers = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Tweet> tweets = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "tweet_likes",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "tweet_id")
+    )
+    private Set<Tweet> likes = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> authorities = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void addTweet(Tweet tweet) {
+        tweets.add(tweet);
+    }
+
+    public void removeTweet(Tweet tweet) {
+        tweets.remove(tweet);
+    }
+
+    public void follow(User user) {
+        following.add(user);
+    }
+
+    public void unfollow(User user) {
+        following.remove(user);
+    }
+
+    public void like(Tweet tweet) {
+        likes.add(tweet);
+    }
+
+    public void unlike(Tweet tweet) {
+        likes.remove(tweet);
+    }
+
+    @Override
+    public String toString() {
+        return "\n----------- User " +
+                "\n id = " + id +
+                "\n name = " + name +
+                "\n userName = " + userName +
+                "\n email = " + email +
+                "\n about = " + about +
+                "\n following = " + following +
+                "\n followers = " + followers +
+                "\n tweets = " + tweets +
+                "\n likes = " + likes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
