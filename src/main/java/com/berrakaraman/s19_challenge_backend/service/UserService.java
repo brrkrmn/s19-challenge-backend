8a4938640582636e9c08e706abf87149bbdc0b09
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-04-30 10:26:41 +0300

    set up user service and add follow logic

--- src/main/java/com/berrakaraman/s19_challenge_backend/service/UserService.java
+++ src/main/java/com/berrakaraman/s19_challenge_backend/service/UserService.java
@@ -1,6 +1,14 @@
 package com.berrakaraman.s19_challenge_backend.service;
 
+import com.berrakaraman.s19_challenge_backend.entity.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService extends UserDetailsService {
+    List<User> findAll();
+    User findById(Long id);
+    User findByUsername(String username);
+    void toggleFollow(Long targetUserId);
 }
--- src/main/java/com/berrakaraman/s19_challenge_backend/service/UserServiceImpl.java
+++ src/main/java/com/berrakaraman/s19_challenge_backend/service/UserServiceImpl.java
@@ -1,23 +1,67 @@
 package com.berrakaraman.s19_challenge_backend.service;
 
+import com.berrakaraman.s19_challenge_backend.entity.Tweet;
+import com.berrakaraman.s19_challenge_backend.entity.User;
+import com.berrakaraman.s19_challenge_backend.exception.BadRequestException;
+import com.berrakaraman.s19_challenge_backend.exception.NotFoundException;
 import com.berrakaraman.s19_challenge_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
+    private AuthenticationService authenticationService;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, AuthenticationService authenticationService) {
         this.userRepository = userRepository;
+        this.authenticationService = authenticationService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findUserByUsername(username)
+        return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User credentials are not valid"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User could not be found with id: " + id));
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new NotFoundException("User could not be found with username: " + username));
+    }
+
+    @Override
+    public void toggleFollow(Long targetUserId) {
+        User authUser = authenticationService.getAuthUser();
+        User targetUser = findById(targetUserId);
+
+        if (authUser.equals(targetUser)) {
+            throw new BadRequestException("Self-following is not allowed.");
+        }
+
+        if (authUser.getFollowing().contains(targetUser)) {
+            authUser.unfollow(targetUser);
+        } else {
+            authUser.follow(targetUser);
+        }
+
+        userRepository.save(authUser);
     }
 }
